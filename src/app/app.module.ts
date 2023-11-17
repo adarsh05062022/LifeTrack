1f6655618475bd2ead9e847214e1f973ef39e938
amansingh7755 <146714908+amansingh7755@users.noreply.github.com>
2023-11-17 16:39:14 +0530

    some changes made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HOMEComponent } from './pages/home/home.component';
+import { PagesModule } from './pages/pages.module';
+import { HomeSliderComponent } from './components/home-slider/home-slider.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavbarComponent, HOMEComponent,HomeSliderComponent],
+  imports: [BrowserModule, AppRoutingModule, ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
+// import { HOMEComponent } from './home/home.component';
+import { NavbarComponent } from '../components/navbar/navbar.component';
+import { HomeSliderComponent } from '../components/home-slider/home-slider.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent,
+    HomeSliderComponent
+  ],
   imports: [
     CommonModule,
     PagesRoutingModule
