e880993ce8906223ffb5eed124677b57d4a83380
Adarsh Dubey <adarshd652@gmail.com>
2023-11-29 22:10:57 +0530

    Some changes made on the last day

---
+++ src/app/pages/Finance/component/finance-header-card/finance-header-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-finance-header-card',
+  templateUrl: './finance-header-card.component.html',
+  styleUrls: ['./finance-header-card.component.scss']
+})
+export class FinanceHeaderCardComponent {
+
+}
