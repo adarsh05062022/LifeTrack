f96a32e0091893342f2db1564efcd144d5335e3a
Adarsh Dubey <adarshd652@gmail.com>
2023-12-01 00:47:53 +0530

    ready for presentation

--- src/app/pages/Finance/component/add-transaction/add-transaction.component.ts
+++ src/app/pages/Finance/component/add-transaction/add-transaction.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-transaction.component.scss']
 })
 export class AddTransactionComponent {
+  expense: any = {};
+
+  submitExpense() {
+    // Perform action on form submission (logging to console in this case)
+    console.log('Submitted Expense:', this.expense);
+    // Clear the form after submission
+    this.resetExpense();
+  }
+
+  resetExpense() {
+    // Reset the expense object to clear the form
+    this.expense = {};
+  }
 
 }
