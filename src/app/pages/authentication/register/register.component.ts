0e2d597761b9df273bc61a4753461155ff4cd9d4
Adarsh Dubey <adarshd652@gmail.com>
2023-12-15 09:29:23 +0530

    make changes

--- src/app/pages/authentication/register/register.component.ts
+++ src/app/pages/authentication/register/register.component.ts
@@ -1,44 +1,52 @@
-import { Component } from '@angular/core';
+// register.component.ts
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthenticationService } from '../services/authentication.service';
 import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   userData = {
     username: '',
     email: '',
     password: ''
   };
 
-  constructor(private authService: AuthenticationService,
-    private router :Router) { }
+  registrationForm!: FormGroup;
+
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit() {
+    this.registrationForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
 
   onSubmit() {
-    
-    console.log('User registration data:', this.userData);
-    this.authService.registerUser(this.userData).subscribe(
-      (response) => {
-        console.log('User registered successfully!', response);
-        // Optionally, perform actions after successful registration
-
-        this.router.navigate(["/login"])
-      },
-      (error) => {
-        alert("Registration Failed")
-        console.error('Registration failed:', error);
-        // Handle registration error
-       
-      }
-    );
+    if (this.registrationForm.valid) {
+      console.log('User registration data:', this.userData);
+      this.authService.registerUser(this.userData).subscribe(
+        (response) => {
+          console.log('User registered successfully!', response);
+          this.router.navigate(['/login']);
+        },
+        (error) => {
+          alert('Registration Failed');
+          console.error('Registration failed:', error);
+        }
+      );
+    } else {
+      this.registrationForm.markAllAsTouched();
+    }
   }
-    
-  }
-
-
-  
-
+}
